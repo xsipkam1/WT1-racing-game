a3a6528665c43645b8f16c38d713d9f3e99c1eb4
115056 <xdubnickas@stuba.sk>
2023-12-18 10:28:38 +0100

    small fixes on  moving road

--- scripts/script.js
+++ scripts/script.js
@@ -27,29 +27,34 @@ function showMenu() {
 
 let roadY=0
 let roadY2=1666
-let speed = 5
+let speed = 8
 
 document.addEventListener("keydown", function(event) {
     console.log(event.code)
     if (event.code === 'KeyW' || event.code === 'ArrowUp') {
         speed = 10;
     } else if (event.code === 'KeyS' || event.code === 'ArrowDown') {
-        speed = 3;
+        speed = 5;
     }
 });
 
 document.addEventListener("keyup", function(event) {
-        speed = 5;
+        speed = 8;
 });
 
 function update() {
 
-    road.clearRoad()
-
-    if(roadY > road.canvas.height) roadY=-road.canvas.height-speed+roadY2+20
+    if(roadY > road.canvas.height) {
+        roadY=-road.canvas.height-speed+roadY2;
+        road.clearRoad();
+    }
     else roadY+=speed
-    if(roadY2 > road2.canvas.height) roadY2=-road2.canvas.height-speed+roadY+20
+    if(roadY2 > road2.canvas.height) {
+        roadY2=-road2.canvas.height-speed+roadY;
+        road2.clearRoad();
+    }
     else roadY2+=speed
+    
     
     road.drawRoad(roadY)
     road2.drawRoad(roadY2)
